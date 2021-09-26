0a1fd5850495a6888ca68d4a7a03e51e2d7ae802
huangguoqiang <huangguoqiang@flashexpress.onaliyun.com>
2021-09-26 21:10:33 +0800

    mapstruct

--- springboot-basic-app/springboot-basic-app-web/src/main/java/org/ganfan/basic/web/mapper/UserMapper.java
+++ springboot-basic-app/springboot-basic-app-web/src/main/java/org/ganfan/basic/web/mapper/UserMapper.java
@@ -4,6 +4,7 @@ import org.ganfan.basic.service.dto.UserParam;
 import org.ganfan.basic.service.dto.UserResult;
 import org.ganfan.basic.web.vo.request.UserQueryReq;
 import org.ganfan.basic.web.vo.response.UserResp;
+import org.mapstruct.Mapper;
 
 import java.util.List;
 
@@ -12,7 +13,7 @@ import java.util.List;
  * @author: huangguoqiang
  * @create: 2021-09-26 16:50
  **/
-
+@Mapper
 public interface UserMapper {
 
     UserParam userQueryReqToUserParam(UserQueryReq userQueryReq);
